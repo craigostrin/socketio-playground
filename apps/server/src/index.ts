29a0ea543ae325b22353ae9e710508622fa2ec66
Craig Ostrin <craigostrin@gmail.com>
2024-02-01 17:55:10 +0100

    feat(server): add socket.io

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,33 @@
 import express, { Request, Response } from 'express'
+import {createServer} from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
 import 'dotenv/config'
 
-console.log(process.env.PORT)
+const PORT = process.env.PORT
 
 const app = express()
-const port = process.env.PORT
+app.use(cors())
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: { origin: ["http://localhost:5173", "http://127.0.0.1:5173"]}
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('message', (message) => {
+    console.log(message)
+    io.emit('message', `${socket.id.slice(0, 2)} said ${message}`)
+  })
+})
+
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
-app.listen(port, () => {
-  console.log(`🤖 BEEP BOOP - Server is running at http://localhost:${port}`)
+httpServer.listen(PORT, () => {
+  console.log(`🤖 BEEP BOOP - Server is running at http://localhost:${PORT}`)
 })
