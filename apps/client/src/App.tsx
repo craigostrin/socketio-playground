e7f0df7331e687374255085262c6b23fc609567b
Craig Ostrin <craigostrin@gmail.com>
2024-02-05 11:02:47 +0100

    feat(client): add basic chat component with socket.io

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,32 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { socket } from './socket'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [messages, setMessages] = useState<String[]>([])
+  const [input, setInput] = useState('')
+
+  function sendMessage() {
+    socket.emit('send', input)
+    setInput('')
+  }
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true)
+    }
+
+    function onDisconnect() {
+      setIsConnected(false)
+    }
+
+    function onReceive(message: String) {
+      setMessages((old) => [...old, message])
+    }
+
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+    socket.on('receive', onReceive)
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+      socket.off('receive', onReceive)
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <div className='p-4 flex flex-col justify-center items-center gap-5'>
+        <h1 className='text-xl'>Chat, I guess</h1>
+        <p>{`You are ${!isConnected ? 'not' : ''} connected`}</p>
+        {messages.map((message, i) => (
+          <p key={i}>{message}</p>
+        ))}
+        <input
+          className='border-black border-2'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
       </div>
-      <h1 className='bg-green-500'>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
